9b0eac748326b50dc31a7ed28ac121fc6c776ff0
Vachagan S. Melikian <vm628@ilab3.cs.rutgers.edu>
2025-02-10 22:24:06 -0500

    added server and client files

---
+++ client.py
@@ -0,0 +1,25 @@
+import socket
+
+def client():
+    try:
+        cs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print("[C]: Client socket created")
+    except socket.error as err:
+        print('socket open error: {} \n'.format(err))
+        exit()
+        
+    # Define the port on which you want to connect to the server
+    port = 50007
+    localhost_addr = socket.gethostbyname(socket.gethostname())
+
+    # connect to the server on local machine
+    server_binding = (localhost_addr, port)
+    cs.connect(server_binding)
+
+    # Receive data from the server
+    data_from_server=cs.recv(100)
+    print("[C]: Data received from server: {}".format(data_from_server.decode('utf-8')))
+
+    # close the client socket
+    cs.close()
+    exit()
---
+++ server.py
@@ -0,0 +1,28 @@
+import socket
+
+def server():
+    try:
+        ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        ss.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        print("[S]: Server socket created")
+    except socket.error as err:
+        print('socket open error: {}\n'.format(err))
+        exit()
+
+    server_binding = ('', 50007)
+    ss.bind(server_binding)
+    ss.listen(1)
+    host = socket.gethostname()
+    print("[S]: Server host name is {}".format(host))
+    localhost_ip = (socket.gethostbyname(host))
+    print("[S]: Server IP address is {}".format(localhost_ip))
+    csockid, addr = ss.accept()
+    print ("[S]: Got a connection request from a client at {}".format(addr))
+
+    # send a intro message to the client.  
+    msg = "Welcome to CS 352!"
+    csockid.send(msg.encode('utf-8'))
+
+    # Close the server socket
+    ss.close()
+    exit()
